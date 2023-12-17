4c1249d2117655402143a8a3f1710245bb84b408
TAKIHamza <hamzataki416@gmail.com>
2023-12-17 04:08:09 +0100

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const commandRoute =require('./routes/commandRoute')
 const productRoutes = require('./routes/productRoute');
 const { router, authMiddleware, blacklistedTokens } = require('./routes/authentification');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 
 require('dotenv').config();
 
@@ -19,7 +23,42 @@ app.use('/images', express.static('images'));
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Routes
-app.use('/auth', router);
+app.post('/signup', async (req, res) => {
+  try {
+    const {name,username, password,rol } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name,username, password: hashedPassword,rol });
+    await user.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Login
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log(req.body)
+    console.log(req.authorization)
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    blacklistedTokens.delete(req.headers.authorization);
+    const token = jwt.sign({ userId: user._id ,userName: user.name}, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ token,user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.use('/product', productRoutes);
 app.use('/command', commandRoute);
 
--- routes/authentification.js
+++ routes/authentification.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const blacklistedTokens = new Set();
+
 
 //middlewar
 const authMiddleware = (req, res, next) => {
@@ -28,41 +28,6 @@ const authMiddleware = (req, res, next) => {
   };
   
 // Signup
-router.post('/signup', async (req, res) => {
-  try {
-    const {name,username, password,rol } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ name,username, password: hashedPassword,rol });
-    await user.save();
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Login
-router.post('/login', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    console.log(req.body)
-    console.log(req.authorization)
-    const user = await User.findOne({ username });
-
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password' });
-    }
-
-    const isValidPassword = await bcrypt.compare(password, user.password);
-    if (!isValidPassword) {
-      return res.status(401).json({ message: 'Invalid username or password' });
-    }
-    blacklistedTokens.delete(req.headers.authorization);
-    const token = jwt.sign({ userId: user._id ,userName: user.name}, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ token,user });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 //logout
 router.post('/logout',authMiddleware, (req, res) => {
