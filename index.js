ab68af674aa2be28318743b35075c4056fdac608
TAKIHamza <hamzataki416@gmail.com>
2023-12-17 03:26:00 +0100

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.use(cors());
 // Middleware
 app.use(bodyParser.json());
